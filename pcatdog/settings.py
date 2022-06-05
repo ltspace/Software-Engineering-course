ae2c01b901a844063bbfb414bbf9ee35d9b4fd41
sdtalt <1409757351@qq.com>
2022-06-06 00:28:52 +0800

    fuck shujvku

--- pcatdog/settings.py
+++ pcatdog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'acatdog',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'pcatdog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,                  # 数据库端口
+        'USER': "root",                # 用户名
+        'PASSWORD': "lt123",           # 密码
+        'NAME': "test",         # 数据库名
     }
 }
 
