b3048fec766a1f29e03b24d29df894e96641786e
sdtalt <1409757351@qq.com>
2022-06-06 00:55:50 +0800

    maybe success

--- acatdog/admin.py
+++ acatdog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import AdoptInfo, AnimalInfo, AssistInfo, ManagerInfo, UserInfo
+admin.site.register([AdoptInfo, AnimalInfo, AssistInfo, ManagerInfo, UserInfo])
--- acatdog/models.py
+++ acatdog/models.py
@@ -1,3 +1,98 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class AdoptInfo(models.Model):
+    # Field name made lowercase.
+    用户id = models.IntegerField(db_column='用户ID', primary_key=True)
+    领养日期 = models.DateTimeField()
+    动物id = models.IntegerField(db_column='动物ID')  # Field name made lowercase.
+    领养单编号 = models.IntegerField()
+    领养状态 = models.CharField(max_length=50)
+    合照 = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'adopt_info'
+
+
+class AnimalInfo(models.Model):
+    # Field name made lowercase.
+    动物id = models.IntegerField(db_column='动物ID', primary_key=True)
+    动物类别 = models.CharField(max_length=10)
+    年龄 = models.IntegerField()
+    照片 = models.TextField(blank=True, null=True)
+    品种 = models.CharField(max_length=11)
+    毛色 = models.CharField(max_length=11)
+    性别 = models.CharField(max_length=11)
+    性格 = models.CharField(max_length=11, blank=True, null=True)
+    地址 = models.CharField(max_length=11)
+    是否被领养 = models.CharField(max_length=11)
+    绝育情况 = models.CharField(max_length=11, blank=True, null=True)
+    疫苗情况 = models.CharField(max_length=11, blank=True, null=True)
+    疾病情况 = models.CharField(max_length=11, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'animal_info'
+
+
+class AssistInfo(models.Model):
+    # Field name made lowercase.
+    动物id = models.IntegerField(db_column='动物ID', primary_key=True)
+    救助日期 = models.DateTimeField()
+    救助单编号 = models.IntegerField()
+    用户id = models.IntegerField(db_column='用户ID')  # Field name made lowercase.
+    是否可被领养 = models.CharField(max_length=2)
+
+    class Meta:
+        managed = True
+        db_table = 'assist_info'
+
+
+class ManagerInfo(models.Model):
+    工号 = models.IntegerField(primary_key=True)
+    性别 = models.CharField(max_length=2)
+    电话 = models.IntegerField()
+    密码 = models.CharField(max_length=10)
+    在线时间 = models.IntegerField()
+    年龄 = models.IntegerField()
+    工龄 = models.IntegerField()
+    投诉邮箱 = models.CharField(max_length=10)
+
+    class Meta:
+        managed = True
+        db_table = 'manager_info'
+
+
+class UserInfo(models.Model):
+    # Field name made lowercase.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # id = models.IntegerField(primary_key=True, unique=True)
+    # username = models.CharField(max_length=20)
+    # pwd = models.CharField(max_length=50)
+    addr = models.CharField(max_length=50, blank=True, null=True)
+    phonum = models.CharField(max_length=13, null=True)
+    sex = models.CharField(max_length=2, default='男', null=True)
+    job = models.CharField(max_length=50, blank=True, null=True)
+    email = models.CharField(max_length=50, null=True)
+    photo = models.URLField(max_length=256, blank=True)
+    age = models.DateTimeField(blank=True, null=True)
+    crepoint = models.IntegerField(default='0')
+    # USERNAME_FIELD = 'id'
+    # REQUIRED_FIELDS = ['username', 'phonum']
+
+    class Meta:
+        managed = True
+        db_table = 'user_info'
+
+    def __str__(self):
+        return str(self.user)
+
+    # @property
+    # def is_authenticated(self):
+    #     """
+    #     Always return True. This is a way to tell if the user has been
+    #     authenticated in templates.
+    #     """
+    #     return True
