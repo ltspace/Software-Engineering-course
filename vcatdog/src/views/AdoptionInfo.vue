86fbe6a84db39299a891c164f58c6ab1bd49e0ae
sdtalt <1409757351@qq.com>
2022-06-09 22:08:12 +0800

    拉大形

--- vcatdog/src/views/AdoptionInfo.vue
+++ vcatdog/src/views/AdoptionInfo.vue
@@ -1,6 +1,112 @@
 <template>
   <UNavBar />
-  <div class="center-in-center">
+  <div class="container center-in-center">
+    <div class="card">
+      <div class="row g-0">
+        <div class="col-md-6">
+          <img src="../assets/left.png" class="img-fluid rounded-start">
+        </div>
+        <div class="col-md-6">
+          <div class="card-body item">
+            <h2 class="card-title" style="margin-bottom: 20px;">Adopt me</h2>
+            <form>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3" value="haha">
+                </div>
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3">
+                </div>
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3" value="haha">
+                </div>
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3" value="haha">
+                </div>
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3" value="haha">
+                </div>
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3" value="haha">
+                </div>
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3" value="haha">
+                </div>
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3" value="haha">
+                </div>
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" id="inputEmail3" value="haha">
+                </div>
+                <button type="button" class="btn btn-dark control col-sm-3">申请领养</button>
+              </div>
+
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+    <!-- 
     <div style="position: absolute;">
       <img src="../assets/left.png" class="d-block w-100" style="border-radius: 10%" alt="...">
       <div class="carousel-caption d-none d-md-block">
@@ -94,10 +200,9 @@
         <div class="col-sm-10">
           <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="已绝育">
         </div>
-      </div>
-    </div>
-</div>
-
+      </div> -->
+    <!-- </div> -->
+  </div>
 </template>
 
 <script>
@@ -109,32 +214,45 @@ export default {
   }
 }
 </script>
+
 <style scoped>
-.azu1 {
-  position: absolute;
-  padding-top: 0em;
-  padding-bottom: 0em;
-}
-
-.azu2 {
-  padding-top: 1em;
-  padding-bottom: 1em;
-}
-
 .a.router-link-exact-active {
   color: black;
 }
 
 .carousel {
-  font-family: YouYuan, Avenir, Helvetica, Arial, sans-serif;
+
   width: 100rem;
   text-align: center;
   border-radius: 20%;
-  transform: scale(1, 1.1);
+
   transform-origin: 0 0;
   font-weight: bolder;
   letter-spacing: 3px;
 }
+
+.card {
+  background: linear-gradient(180deg, rgba(251, 200, 212, 1) 0%, rgba(151, 149, 240, 1) 100%);
+  box-shadow: 0px 20px 20px rgba(0, 0, 0, 0.33);
+  border-radius: 72px;
+  border: 0ch;
+
+}
+
+.col-form-label {
+  font-size: large;
+}
+
+.item {
+  margin-top: 5%;
+}
+
+.form-control {
+  background: transparent;
+  font-size: large;
+  border-width: 0;
+}
+
 
 .center-in-center {
   position: absolute;
@@ -147,22 +265,21 @@ export default {
   transform: translate(-50%, -50%);
 }
 
-.azu3 {
-  position: absolute;
-  left: 900px;
-  top: 150px;
+.control {
+  border-radius: 10px;
+  letter-spacing: 5px;
+  text-align: center;
 }
 
-.azu4 {
-  position: absolute;
-  left: 1100px;
-  top: 200px;
+.control:hover {
+  box-shadow: 2px 2px 10px grey;
+  transition: 500ms;
 }
 
-.azu5 {
-  position: absolute;
-  left: 1050px;
-  top: 580px;
+
+.form-group {
+  margin-top: 5px;
+  margin-bottom: 5px;
 }
 </style>
 
