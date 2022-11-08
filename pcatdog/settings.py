5dc8296dea6ced71cfefa553857905eb3265a1c0
LT-er <71513206+LT-er@users.noreply.github.com>
2022-11-08 19:21:06 +0800

    delete secret key.......我是伞兵

--- pcatdog/settings.py
+++ pcatdog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o+=6*-0$a6acrq@g_17s+2mktq5j*k*elrl$3eo7v&($7u)&m&'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
